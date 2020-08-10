18ef1ea4e632a04b03ad24b4e982800316b00b5b
jklom0326 <jklom0326@gmail.com>
2020-08-11 03:40:03 +0900

    08/11

---
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.instagramclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
