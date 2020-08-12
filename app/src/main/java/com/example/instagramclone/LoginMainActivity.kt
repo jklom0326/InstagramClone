3534a0b5a8a9ab897037b6159cf32293d89f1fb8
Sangwan <jklom0326@gmail.com>
2020-08-12 21:07:55 +0900

    08/12/

--- app/src/main/java/com/example/instagramclone/LoginMainActivity.kt
+++ app/src/main/java/com/example/instagramclone/LoginMainActivity.kt
@@ -1,49 +1,54 @@
 package com.example.instagramclone
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
+import android.widget.Button
 
 class LoginMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginmain)
 
-        val provider = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.FacebookBuilder().build()
-        )
+        lateinit var Button1: Button
+        Button1 = findViewById(R.id.button1)
 
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(provider)
-                .build(),
-            RC_SIGN_IN
-        )
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SIGN_IN){
-            val response = IdpResponse.fromResultIntent(data)
-
-            if (resultCode == Activity.RESULT_OK) {
-                // 로그인 성공 했을 때
-                val user = FirebaseAuth.getInstance().currentUser
-
-            }else{
-                // 로그인 실패 했을 때
-            }
+        Button1.setOnClickListener{
+            val intent =Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
         }
+
+//        val provider = arrayListOf(
+//            AuthUI.IdpConfig.EmailBuilder().build(),
+//            AuthUI.IdpConfig.PhoneBuilder().build(),
+//            AuthUI.IdpConfig.FacebookBuilder().build()
+//        )
+
+//        startActivityForResult(
+//            AuthUI.getInstance()
+//                .createSignInIntentBuilder()
+//                .setAvailableProviders(provider)
+//                .build(),
+//            RC_SIGN_IN
+//        )
     }
-    companion object {
-        private const val RC_SIGN_IN = 1001
-    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == RC_SIGN_IN){
+//            val response = IdpResponse.fromResultIntent(data)
+//
+//            if (resultCode == Activity.RESULT_OK) {
+//                // 로그인 성공 했을 때
+//                val user = FirebaseAuth.getInstance().currentUser
+//
+//            }else{
+//                // 로그인 실패 했을 때
+//            }
+//        }
+//    }
+//    companion object {
+//        private const val RC_SIGN_IN = 1001
+//    }
 }
---
+++ app/src/main/java/com/example/instagramclone/RegisterActivity.kt
@@ -0,0 +1,36 @@
+package com.example.instagramclone
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+//        printHashKey()
+    }
+    //HashKEY =  4SX63uPmeroz6HB7B4NZp4gEoS0=
+
+    fun printHashKey() {
+        try {
+            val info: PackageInfo = getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("TAG", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("TAG", "printHashKey()", e)
+        }
+    }
+}
