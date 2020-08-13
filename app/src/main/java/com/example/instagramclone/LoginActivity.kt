9113715923683dc6a1df2474261e3ada26476390
Sangwan <jklom0326@gmail.com>
2020-08-13 21:35:44 +0900

    08/12/

--- app/src/main/java/com/example/instagramclone/LoginActivity.kt
+++ app/src/main/java/com/example/instagramclone/LoginActivity.kt
@@ -1,11 +1,80 @@
 package com.example.instagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckedTextView
+import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? =null
+    var callbackManger : CallbackManager? =null
+    lateinit var checkedTextView :CheckedTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        callbackManger = CallbackManager.Factory.create()
+        checkedTextView = findViewById(R.id.checkedTextView)
+
+        checkedTextView.setOnClickListener {
+            facebookLogin()
+        }
+    }
+
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManger, object : FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onError(error: FacebookException?) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+    }
+
+    fun handleFacebookAccessToken(token : AccessToken?){
+        val credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                task ->
+                if (task.isSuccessful){
+                    moveMainPage(task.result?.user)
+                }else{
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManger?.onActivityResult(requestCode,resultCode,data)
+    }
+
+    fun moveMainPage(user:FirebaseUser?){
+        if (user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
